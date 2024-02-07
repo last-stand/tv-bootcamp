5a30ed884b281b424d1504823886b7aa3e7d3acb
Jai P <jai.prakash@techverito.com>
2024-02-07 13:19:49 +0530

    Updating Currency enum to class

--- src/main/java/org/example/wallet/Currency.java
+++ src/main/java/org/example/wallet/Currency.java
@@ -1,5 +1,9 @@
 package org.example.wallet;
 
-public enum Currency {
-    INR, EUR, USD
+public class Currency {
+    public static final Currency INR = new Currency(1);
+    public static final Currency USD = new Currency(2);
+    public static final Currency EUR = new Currency(4);
+    public Currency(int conversionRate) {
+    }
 }
